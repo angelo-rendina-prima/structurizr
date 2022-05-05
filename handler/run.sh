8a3176730101f9696b857ca891dbca8777319dd2
Angelo <angelo.rendina@prima.it>
2022-05-05 09:49:47 +0100

    Ciao mamma

--- handler/run.sh
+++ handler/run.sh
@@ -1,9 +1,9 @@
 set -ex
 
 while true; do
-    INPUT = $(nc -l -p 6113)
-    NAME = $(echo $INPUT | head -n 1)
-    CONTENT = $(echo $INPUT | tail -n +2)
+    INPUT=$(nc -l 6113)
+    NAME=$(echo $INPUT | head -n 1)
+    CONTENT=$(echo $INPUT | tail -n +2)
     echo $CONTENT > "structurizr/$NAME"
 
     git add .
